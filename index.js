40deccb0812d8c67fb5dfe3621a5319c15339ba2
ozzy <iturturika89@gmail.com>
2023-10-10 19:47:09 +0300

    change port to 8080

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ app.get('/items', getAll);
 
 app.patch('/items/:id', checkAuth, upload.array('images', 4), itemValidation, updateItem);
 
-app.listen(443, (err) => {
+app.listen(8080, (err) => {
     if(err){
         return console.log(err);
     } else {
